0dbf7476ac0e94011c106f53a8dd9eb5ff46bf7b
Daisuke Baba <baba.daisuke@gmail.com>
2016-01-24 13:27:36 +0900

    Rename filtered files in order to avoid to unexpectedly  commit filtered files

--- install.sh
+++ install.sh
@@ -2,6 +2,11 @@
 
 VERSION=0.9.5
 
+RECORD="./files.txt"
+if [ -n "$1" ]; then
+  RECORD=$1
+fi
+
 function assert_root {
   if [[ $EUID -ne 0 ]]; then
      echo "This script must be run as root" 
@@ -10,9 +15,11 @@ function assert_root {
 }
 
 function install {
+  cp -f PKG-INFO.txt PKG-INFO
   sed -i -e "s/%VERSION%/${VERSION//\//\\/}/g" PKG-INFO
+  cp -f setup.py.txt setup.py
   sed -i -e "s/%VERSION%/${VERSION//\//\\/}/g" setup.py
-  python ./setup.py install
+  python ./setup.py install --record ${RECORD}
 }
 
 assert_root
